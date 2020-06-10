60d05924130d19e6927d28299db0cbd147d64926
TheoNorris <theonorris@live.com>
2020-06-10 16:57:15 +0000

    Added starting photo and introduction text, styled texts and coloured backgrounds

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,13 @@
 
 function artistInformationHTML(id_artist) {
 
-var instagram = id_artist.result.instagram; 
-
- console.log(instagram);
-    
-
 return $("#artist-name").html(id_artist.result.artist),
-$("#artist-image").attr("src", id_artist.result.cover),
+$("#artist-image-div").css("background", `url(${id_artist.result.cover}) no-repeat center center`),
 $("#facebook").attr("href", "https://www.facebook.com/" + id_artist.result.facebook),
 $("#instagram").attr("href", "https://www.instagram.com/" + id_artist.result.instagram),
-$("#spotify").attr("href", "https://open.spotify.com/search/" + id_artist.result.artist); 
+$("#spotify").attr("href", "https://open.spotify.com/search/" + id_artist.result.artist), 
+$("#twitter").attr("href", "https://www.twitter.com/" + id_artist.result.twitter),
+$("#youtube").attr("href", "https://open.youtube.com/search/" + id_artist.result.youtube); 
 }
 
 
@@ -29,15 +26,16 @@ artistAlbums = id_artist.result.albums;
 for (i = 0; i < artistAlbums.length && i < 5; i++) {
 
 var artistCoverString = artistAlbums[i].cover;
+var albumLinks = artistAlbums[i].album.split()
 var artistCovers = artistCoverString.split(" ");
 
 var individualAlbumArt;
 
-for (j = 0; j < artistCovers.length; j++) {
+for (j = 0, h = 0; j < artistCovers.length, h < albumLinks.length; j++, h++) {
 
-$(".album-img").attr("src", artistCovers[j]);
 
-individualAlbumArt += '<img class="album-img" src="' + artistCovers[j] + '" class="album-img">';
+individualAlbumArt += `<a class="album-links" href="https://open.spotify.com/search/${id_artist.result.artist} ${albumLinks[h]}" target="_blank"><img class="album-img" src="${artistCovers[j]}" class="album-img"></a>`;
+
 
 var albumArtToImg = individualAlbumArt.replace("undefined", "");}
 
@@ -47,20 +45,23 @@ document.getElementById("album-images").innerHTML = albumArtToImg;
 
 function getArtist() {
 
+
 var artistBio = $("#artists option:selected").val(),
     artistValue = $("#artists option:selected").val(); 
- 
-if (artistValue === "Kano") {
- artistValue = "64731";
-} else if (artistValue === "Wretch 32") {
-  artistValue = "25889"; 
+
+if (artistValue === "AJ Tracey") {
+ artistValue = "23599";
+} else if (artistValue === "Dream Mclean") {
+  artistValue = "116406"; 
 } else if (artistValue === "Kojey Radical") {
   artistValue = "23636";
 } else if (artistValue === "Klashnekoff") {
   artistValue = "106201"; 
-} else if (artistValue === "Coops") {
- return artistValue = "152363"; 
-} 
+} else if (artistValue === "Knucks") {
+  artistValue = "81352"; 
+} else {
+    return;
+}
 
 $.when(
   $.getJSON(`https://api.happi.dev/v1/music/artists/${artistValue}?apikey=014a1a2mKrOybg41LnU3DQB8IJhBQS29KaeVLZdEB6UI5DUHzx3qlyls`),
@@ -86,4 +87,5 @@ $.when(
     }
   });
 }
+
 
